cddf8d6a913b993f6f2d9e28f45377cead94551f
Karol Tatiana Ibarra <kt.ibarra@unimayor.edu.co>
2026-05-17 08:52:14 -0500

    cambios finales de los finales de prubeas finales

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-  res.send("CI/CD funcionando correctamente :) ");
+  res.send("CI/CD funcionando correctamente :)  KAROL IBARRA");
   res.send("PRUEBA :) ");
 
 });
