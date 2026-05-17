aa34b7f0f6f9e862667dc4f737f119c6c360d0ef
Karol Tatiana Ibarra <kt.ibarra@unimayor.edu.co>
2026-05-16 23:49:32 -0500

    cambios de pruebas finales

--- server.js
+++ server.js
@@ -7,5 +7,7 @@ app.get("/", (req, res) => {
 
 app.listen(3000, () => {
   console.log("ESTA ES LA PRIMERA PRUEBA");
+  console.log("ESTA ES LA SEGUNDA PRUEBA");
+  console.log("ESTA ES LA TERCERA PRUEBA");
   console.log("Servidor en puerto 3000");
 });
