c775c172dd69d6f305030820ec4113371584236e
Karol Tatiana Ibarra <kt.ibarra@unimayor.edu.co>
2026-05-16 23:52:57 -0500

    cambios finales final

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Hola CI/CD funcionando ");
+  res.send("Hola CI/CD funcionando (PRUEBAS FINALES ,FINALES) ");
 });
 
 app.listen(3000, () => {
