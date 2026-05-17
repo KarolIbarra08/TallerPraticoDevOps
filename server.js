3c319b11dac914d622f5649fc4284f1cced01f19
Karol Tatiana Ibarra <kt.ibarra@unimayor.edu.co>
2026-05-17 08:31:29 -0500

    cambios y cambios y cambios

--- server.js
+++ server.js
@@ -4,12 +4,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-  res.send("CI/CD funcionando correctamente ");
-  res.send("PRUEBA 1 ");
-  res.send("PRUEBA 2 ");
-  res.send("PRUEBA 3 ");
-  
-
+  res.send("CI/CD funcionando correctamente :) ");
 });
 
 app.listen(PORT, () => {
